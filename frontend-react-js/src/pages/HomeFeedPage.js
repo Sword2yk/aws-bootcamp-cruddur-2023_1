b0adf4b1ce393598a8a255a4502d4efba267e690
Chinedu <obichinedu@gmail.com>
2023-03-10 15:48:25 +0000

    Backend verification and token checks

--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -1,7 +1,6 @@
 import './HomeFeedPage.css';
 import React from "react";
 
-//AWS Amplify
 import { Auth } from 'aws-amplify';
 
 import DesktopNavigation  from '../components/DesktopNavigation';
@@ -24,59 +23,42 @@ export default function HomeFeedPage() {
   const loadData = async () => {
     try {
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/home`
-      var startTime = performance.now()
       const res = await fetch(backend_url, {
         headers: {
           Authorization: `Bearer ${localStorage.getItem("access_token")}`
         },
         method: "GET"
       });
-      var endTime = performance.now()
-
       let resJson = await res.json();
       if (res.status === 200) {
         setActivities(resJson)
-        tracer.startActiveSpan('HomeFeedPageLoadData', hmfSpan => {
-          // Add attributes to custom span
-          hmfSpan.setAttribute('homeFeedPage.latency_MS', (endTime - startTime));
-          hmfSpan.setAttribute('homeFeedPage.status', true);
-          hmfSpan.end();
-        });
       } else {
         console.log(res)
-        tracer.startActiveSpan('HomeFeedPageLoadData', hmfSpan => {
-          // Add attributes to custom span
-          hmfSpan.setAttribute('homeFeedPage.latency_MS', (endTime - startTime));
-          hmfSpan.setAttribute('homeFeedPage.status', false);
-          hmfSpan.end();
-        });
       }
     } catch (err) {
       console.log(err);
     }
   };
 
-  // check if we are authenicated
-const checkAuth = async () => {
-  Auth.currentAuthenticatedUser({
-    // Optional, By default is false. 
-    // If set to true, this call will send a 
-    // request to Cognito to get the latest user data
-    bypassCache: false 
-  })
-  .then((user) => {
-    console.log('user',user);
-    return Auth.currentAuthenticatedUser()
-  }).then((cognito_user) => {
-      setUser({
-        display_name: cognito_user.attributes.name,
-        handle: cognito_user.attributes.preferred_username
-      })
-  })
-  .catch((err) => console.log(err));
-};
-
-
+  const checkAuth = async () => {
+    Auth.currentAuthenticatedUser({
+      // Optional, By default is false. 
+      // If set to true, this call will send a 
+      // request to Cognito to get the latest user data
+      bypassCache: false 
+    })
+    .then((user) => {
+      console.log('user',user);
+      return Auth.currentAuthenticatedUser()
+    }).then((cognito_user) => {
+        setUser({
+          display_name: cognito_user.attributes.name,
+          handle: cognito_user.attributes.preferred_username
+        })
+    })
+    .catch((err) => console.log(err));
+  };
+  
   React.useEffect(()=>{
     //prevents double call
     if (dataFetchedRef.current) return;
